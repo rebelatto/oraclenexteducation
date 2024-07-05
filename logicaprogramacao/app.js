10cec7dfa3c2cf8809b6da345add3480719321d4
Jonison Rebelatto Moura Barbosa <jonisonrebelatto@gmail.com>
2024-07-05 11:31:13 -0300

    refactor: uso de ternário

--- logicaprogramacao/app.js
+++ logicaprogramacao/app.js
@@ -1,4 +1,4 @@
-alert("Boas vindas ao Jogo do Número Secreto!");
+alert('Boas vindas ao Jogo do Número Secreto!');
 
 let numeroSecreto = 8;
 console.log(numeroSecreto);
@@ -6,10 +6,10 @@ let chute;
 let tentativas = 1;
 
 while (chute != numeroSecreto){
-	chute = prompt("Escolha o numero de 1 a 10: ");
+	chute = prompt('Escolha o numero de 1 a 10: ');
 
 	if (chute == numeroSecreto){
-	 	alert(`Parabéns, você descobriu o número secreto ${numeroSecreto}! Tentativas usadas ${tentativas}`);	
+	 	break;
 	}else {
 		if (chute > numeroSecreto) {
 			alert(`O número secreto é menor que ${chute}.`);
@@ -18,6 +18,10 @@ while (chute != numeroSecreto){
 			alert(`O número secreto é maior que ${chute}.`);
 		}
 	}
-
 	tentativas++;
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+
+alert(`Parabéns, você descobriu o número secreto ${numeroSecreto} em ${tentativas} ${palavraTentativa}`);	
+
