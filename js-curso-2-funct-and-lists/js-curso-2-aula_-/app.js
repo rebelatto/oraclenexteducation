7cf5ad18a34cdb42f2dd4272ca904e5c61a89d8b
Jonison Rebelatto Moura Barbosa <jonisonrebelatto@gmail.com>
2024-07-08 16:31:39 -0300

    feat:  criando as funções
    Criação das Funções  para gerar o numero aleatorio e verificar os chutes.

--- js-curso-2-funct-and-lists/js-curso-2-aula_-/app.js
+++ js-curso-2-funct-and-lists/js-curso-2-aula_-/app.js
@@ -1,20 +1,51 @@
+let maximo = 10;
+let tentativa = 1;
+let numeroSecreto = gerarNumeroAleatorio();
 
-let titulo = document.querySelector('h1');
+exibirMensagemInicial();
 
-titulo.innerHTML = 'Jogo do Número Secreto';
-
-let paragrafo = document.querySelector('p');
-
-let maximo =  10;
-
-paragrafo.innerHTML = `Escolha um número entre 1 e ${maximo}`;
-
-let numeroSecreto = parseInt(Math.random()* maximo + 1);
-
-function verificarChute(){
-	console.log('chute verificado');
+function exibirTextoNaTela(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
 }
 
-function reiniciar(){
-
+function exibirMensagemInicial() {
+  exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+  exibirTextoNaTela('p', `Escolha um número entre 1 e ${maximo}`);
 }
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * maximo + 1);
+}
+
+function limparCampo() {
+  chute = document.querySelector('input');
+  chute.value = '';
+}
+
+function verificarChute() {
+  let chute = document.querySelector('input').value;
+
+  if (chute == numeroSecreto) {
+    let palavraTentativa = tentativa > 1 ? 'tentativas' : 'tentativa';
+    let mensagemTentativa = `Você descobriu o número secreto com ${tentativa} ${palavraTentativa}`;
+    exibirTextoNaTela('h1', 'Acertou!');
+    exibirTextoNaTela('p', mensagemTentativa);
+    document.getElementById('reiniciar').removeAttribute('disabled');
+  } else if (chute > numeroSecreto) {
+    exibirTextoNaTela('p', 'O Número Secreto é Menor.');
+  } else {
+    exibirTextoNaTela('p', 'O Número Secreto é Maior.');
+  }
+  tentativa++;
+  limparCampo();
+}
+
+function reiniciarJogo() {
+  exibirMensagemInicial();
+  document.getElementById('reiniciar').setAttribute('disabled', true);
+  numeroSecreto = gerarNumeroAleatorio();
+  tentativa = 1;
+}
+
+exibirMensagemInicial();
