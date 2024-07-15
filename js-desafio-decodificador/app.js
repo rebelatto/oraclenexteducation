e7a7518f8fb16eeb18c541a2f223ecaa2d6121f4
Jonison Rebelatto Moura Barbosa <jonisonrebelatto@gmail.com>
2024-07-15 16:34:49 -0300

    style: ajustando container encrypt

--- js-desafio-decodificador/app.js
+++ js-desafio-decodificador/app.js
@@ -1,7 +1,11 @@
 function encrypt() {
   let encrypted = document.querySelector('textarea').value;
   let elemento = document.getElementById('encrypted');
-  elemento.innerHTML = encrypted;
+  elemento.innerHTML =
+    encrypted +
+    `<button class="btn-decrypt" onclick="copy()">
+        Copiar
+      </button>`;
   console.log(elemento);
   console.log(encrypted);
 }
